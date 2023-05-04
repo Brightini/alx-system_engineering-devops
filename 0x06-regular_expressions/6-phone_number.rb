ee18b688502b8e363942c2f699145bbc0a8a44c8
Brightini <brightiniabasi@gmail.com>
2023-05-04 05:50:00 -0700

    Fixed regex bug for numbers of digits > 10 :bug:

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -2,7 +2,8 @@
 # This script matches characters
 
 arg = ARGV[0] # gets first argument from terminal
-regex = /^\d{10}/ # creates search pattern
+regex = /^\d{10}(?!\d)/ # creates search pattern
 
-matches = arg.scan(regex)
-puts matches.join("")
+result = arg.scan(regex).join("")
+
+puts result.length > 10 ? "" : result
