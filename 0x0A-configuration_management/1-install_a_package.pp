658cbd73e335516b5159f97202989af7da1cdf24
Brightini <brightiniabasi@gmail.com>
2023-06-23 23:35:43 +0100

    Minor update :zap:

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # This manifest installs a package, flask
 package { 'flask'
-    name   => 'flask-2.1.0'
-    ensure => 'installed'
-    source => 'pip3'
+    name   => 'flask-2.1.0',
+    ensure => 'installed',
+    source => 'pip3',
 }
