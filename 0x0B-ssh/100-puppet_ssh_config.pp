bd6c1cfd16a4bdcbdc6634b9ab5683be2314b05a
Brightini <brightiniabasi@gmail.com>
2023-06-28 07:10:18 -0700

    Refactor code :hammer:

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,13 @@
 # This manifest makes changes to the configuration file of a client
 
-Host *
-	IdentityFile ~/.ssh/school
-	PasswordAuthentication no
-	HostbasedAuthentication no
+file_line { 'turning off password-based authenticationn':
+    ensure => 'present',
+    path   => '/etc/ssh/ssh_config',
+    line   => 'IdentityFile ~/.ssh/school',
+}
+
+file_line {
+    ensure => 'present',
+    path   => '/etc/ssh/ssh_config',
+    line   => 'PasswordAuthentication no',
+}
