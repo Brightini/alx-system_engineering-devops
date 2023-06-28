d140b94f3331d9446f1d414f4ba409e2cb02b52b
Brightini <brightiniabasi@gmail.com>
2023-06-28 04:06:51 -0700

    Updated code

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,5 @@
 # This manifest makes changes to the configuration file of a client
 
-Host {54.236.25.169:
+Host 54.236.25.169
 	IdentityFile '~/.ssh/school'
-	PasswordAuthentication no
-}
+	PasswordAuthentication no}
